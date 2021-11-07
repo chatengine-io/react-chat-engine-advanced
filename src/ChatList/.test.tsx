1a99b69d71eb9cc52637bf1d0d1135efb4e68d57
Adam La Morre <alamorre@gmail.com>
2021-11-06 17:42:50 -0700

    added chats props

--- src/ChatList/.test.tsx
+++ src/ChatList/.test.tsx
@@ -5,7 +5,7 @@ import { Default as Thing } from './stories';
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Thing chats={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
