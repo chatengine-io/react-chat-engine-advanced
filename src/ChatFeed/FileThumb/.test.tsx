b9c2a97402f1d03bf857de656075344294b539aa
Adam La Morre <alamorre@gmail.com>
2021-11-08 15:30:46 -0800

    tests pass

--- src/ChatFeed/FileThumb/.test.tsx
+++ src/ChatFeed/FileThumb/.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from './stories';
+
+import { FileThumb as Thing } from '.';
+
+import { fileAttachment } from '../../util/mocks';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Thing attachment={fileAttachment} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
