40596f0dd8df5e87fbf8609c39c57dc6ab7c632b
Adam La Morre <alamorre@gmail.com>
2021-11-09 17:12:16 -0800

    date time added (#14)

---
+++ src/ChatFeed/DateTime/styles.tsx
@@ -0,0 +1,13 @@
+import { Properties } from 'csstype';
+
+export const styles = {
+  dateText: {
+    width: '100%',
+    fontFamily: 'Avenir',
+    textAlign: 'center',
+    paddingTop: '4px',
+    paddingBottom: '10px',
+    fontSize: '15px',
+    color: 'rgba(0, 0, 0, .40)',
+  } as Properties,
+};
