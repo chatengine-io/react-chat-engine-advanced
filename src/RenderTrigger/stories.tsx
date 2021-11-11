b2419bd1842596c4a77836996873d2064d7d8815
Adam La Morre <alamorre@gmail.com>
2021-11-11 07:26:03 -0800

    Feat render trigger refactor (#17)
    * ready to abstract into message
    
    * change again - rip the folder structure bandaid off
    
    * simple message component
    
    * docs according to top-level message
    
    * no more My/Their message dirs
    
    * ready to make styles interface
    
    * ready to doc custom style
    
    * messages styles docs are addex
    
    * ChatLoader -> RenderTrigger

---
+++ src/RenderTrigger/stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Meta, Story } from '@storybook/react';
+
+import { LoadingOutlined } from '@ant-design/icons';
+
+import { RenderTrigger } from '.';
+import { Props } from './props';
+
+const meta: Meta = {
+  title: 'RenderTrigger',
+  component: RenderTrigger,
+  argTypes: {},
+};
+
+export default meta;
+
+const Template: Story<Props> = (props) => (
+  <RenderTrigger {...props}></RenderTrigger>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  onShow: () => console.log('Chat Loader is visible.'),
+  onHide: () => console.log('Chat Loader is NOT visible.'),
+  style: { maxWidth: '400px' },
+};
+
+export const CustomStyles = Template.bind({});
+CustomStyles.args = {
+  onShow: () => console.log('Chat Loader is visible.'),
+  onHide: () => console.log('Chat Loader is NOT visible.'),
+  style: {
+    maxWidth: '400px',
+    backgroundColor: '#4a5162',
+    fontFamily: 'Avenir',
+    textAlign: 'center',
+    margin: '4px',
+    paddingTop: '14px',
+    paddingBottom: '14px',
+    borderRadius: '4px',
+  },
+  children: <LoadingOutlined style={{ fontSize: '21px', color: '#f0f0f0' }} />,
+};
--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,4 @@ export * from './Dot';
 
 export * from './ChatList';
 export * from './ChatList/ChatCard';
-export * from './ChatList/ChatForm';
-export * from './ChatList/ChatLoader';
+export * from './RenderTrigger';
