3cf4b29181fb9ea4a8134634e7de99dafba50e84
Adam La Morre <alamorre@gmail.com>
2021-11-11 17:57:19 -0800

    chat header plus tests and stories

--- src/ChatFeed/ChatHeader/index.tsx
+++ src/ChatFeed/ChatHeader/index.tsx
@@ -1,1 +1,70 @@
-import React from 'react';import { Props } from './props';export const index: React.FC<Props> = ({}) => {return <div />;};
+import React from 'react';
+
+import { Props } from './props';
+import { styles } from './styles';
+
+import { Row, Col, setConfiguration } from 'react-grid-system';
+
+setConfiguration({ maxScreenClass: 'xl', gutterWidth: 0 });
+
+export const ChatHeader: React.FC<Props> = ({
+  id = '',
+  title = '',
+  description = '',
+  style = {},
+}) => {
+  return (
+    <Row
+      className="ce-chat-title"
+      style={{ ...styles.titleSection, ...style.titleSection }}
+    >
+      <Col
+        xs={2}
+        sm={0}
+        style={{
+          ...styles.mobileOptiom,
+          ...{ left: '6px' },
+          ...style.mobileOptiom,
+        }}
+        className="ce-chat-list-mobile-option"
+      >
+        {/* <ChatListDrawer /> */}
+      </Col>
+
+      <Col
+        xs={8}
+        sm={12}
+        style={{ ...styles.titleContainer, ...style.titleContainer }}
+        className="ce-chat-title-container"
+      >
+        <div
+          style={{ ...styles.titleText, ...style.titleText }}
+          className="ce-chat-title-text"
+          id={`ce-chat-feed-title-${id}`}
+        >
+          {title}
+        </div>
+
+        <div
+          style={{ ...styles.subtitleText, ...style.subtitleText }}
+          className="ce-chat-subtitle-text"
+        >
+          {description}
+        </div>
+      </Col>
+
+      <Col
+        xs={2}
+        sm={0}
+        style={{
+          ...styles.mobileOptiom,
+          ...{ right: '6px' },
+          ...style.mobileOptiom,
+        }}
+        className="ce-chat-settings-mobile-option"
+      >
+        {/* <ChatSettingsDrawer /> */}
+      </Col>
+    </Row>
+  );
+};
--- src/ChatFeed/ChatHeader/props.tsx
+++ src/ChatFeed/ChatHeader/props.tsx
@@ -1,1 +1,8 @@
-export interface Props {}
+import { ChatHeaderStyles } from './styles';
+
+export interface Props {
+  id?: string | number;
+  title?: string;
+  description?: string;
+  style?: ChatHeaderStyles;
+}
