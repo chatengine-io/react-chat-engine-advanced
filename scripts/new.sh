9e26f44530d63be3c0d669bb9e0b08f7f1197ff1
Dylan Havelock <dylan.havelock@gmail.com>
2021-11-13 16:24:44 -0500

    update new component script (#21)

--- scripts/new.sh
+++ scripts/new.sh
@@ -2,8 +2,52 @@
 
 mkdir ./src/$1
 
-echo "import React from 'react';import { Props } from './props';export const index: React.FC<Props> = ({}) => {return <div />;};" > ./src/$1/index.tsx
+COMPONENT_NAME=`basename $1`
+
+# Index
+echo "import React from 'react';" > ./src/$1/index.tsx
+echo "import { Props } from './props';" >> ./src/$1/index.tsx
+echo "" >> ./src/$1/index.tsx
+echo "export const $COMPONENT_NAME: React.FC<Props> = ({}: Props) => {" >> ./src/$1/index.tsx
+echo "  return <div />;" >> ./src/$1/index.tsx
+echo "};" >> ./src/$1/index.tsx
+
+# Props
 echo "export interface Props {}" > ./src/$1/props.tsx
-echo "" > ./src/$1/stories.mdx
-echo "import { Properties } from 'csstype';export const styles = {example: {} as Properties,};" > ./src/$1/styles.tsx
-echo "" > ./src/$1/.test.tsx
+
+# Stories
+echo "import { ArgsTable, Meta, Story, Canvas } from '@storybook/addon-docs';" > ./src/$1/stories.mdx
+echo "import { $COMPONENT_NAME } from '.';" >> ./src/$1/stories.mdx
+echo "import { Props } from './props';" >> ./src/$1/stories.mdx
+echo "" >> ./src/$1/stories.mdx
+echo "<Meta title=\"$COMPONENT_NAME\" component={$COMPONENT_NAME} />" >> ./src/$1/stories.mdx
+echo "" >> ./src/$1/stories.mdx
+echo "export const Template = (args) => <$COMPONENT_NAME {...args} />;" >> ./src/$1/stories.mdx
+echo "" >> ./src/$1/stories.mdx
+echo "# $COMPONENT_NAME" >> ./src/$1/stories.mdx
+echo "" >> ./src/$1/stories.mdx
+echo "## Default" >> ./src/$1/stories.mdx
+echo "" >> ./src/$1/stories.mdx
+echo "<Story name='Default' args={{}}>" >> ./src/$1/stories.mdx
+echo "  {Template.bind({})}" >> ./src/$1/stories.mdx
+echo "</Story>" >> ./src/$1/stories.mdx
+
+# Style
+echo "import { Properties } from 'csstype';" > ./src/$1/styles.tsx
+echo "" >> ./src/$1/styles.tsx
+echo "export const styles = {" >> ./src/$1/styles.tsx
+echo "  example: {} as Properties," >> ./src/$1/styles.tsx
+echo "};" >> ./src/$1/styles.tsx
+
+# Test
+echo "import React from 'react';" > ./src/$1/.test.tsx
+echo "import * as ReactDOM from 'react-dom';" >> ./src/$1/.test.tsx
+echo "import { $COMPONENT_NAME as Thing } from '.';" >> ./src/$1/.test.tsx
+echo "" >> ./src/$1/.test.tsx
+echo "describe('Thing', () => {" >> ./src/$1/.test.tsx
+echo "  it('renders without crashing', () => {" >> ./src/$1/.test.tsx
+echo "    const div = document.createElement('div');" >> ./src/$1/.test.tsx
+echo "    ReactDOM.render(<Thing />, div);" >> ./src/$1/.test.tsx
+echo "    ReactDOM.unmountComponentAtNode(div);" >> ./src/$1/.test.tsx
+echo "  });" >> ./src/$1/.test.tsx
+echo "});" >> ./src/$1/.test.tsx
