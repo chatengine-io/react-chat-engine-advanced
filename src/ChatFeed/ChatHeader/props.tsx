b9ddeddc36ce40874b74037a9b51ea201416f43c
Adam La Morre <alamorre@gmail.com>
2021-11-13 13:23:53 -0800

    Feat: chat header (#19)
    * basic message list added
    
    * ready to add is my message
    
    * message list complete with docs
    
    * code clean
    
    * render triggers
    
    * npm new files
    
    * chat header plus tests and stories
    
    * mobileOption

---
+++ src/ChatFeed/ChatHeader/props.tsx
@@ -0,0 +1,8 @@
+import { ChatHeaderStyles } from './styles';
+
+export interface Props {
+  id?: string | number;
+  title?: string;
+  description?: string;
+  style?: ChatHeaderStyles;
+}
---
+++ src/ChatFeed/ChatHeader/styles.tsx
@@ -0,0 +1,40 @@
+import { Properties } from 'csstype';
+
+export interface ChatHeaderStyles {
+  titleSection?: Properties;
+  mobileOption?: Properties;
+  titleContainer?: Properties;
+  titleText?: Properties;
+  subtitleText?: Properties;
+}
+
+export const styles = {
+  titleSection: {
+    width: '100%',
+    zIndex: 1,
+    backgroundColor: 'rgb(256, 256, 256, 0.92)',
+  } as Properties,
+  mobileOption: {
+    width: '100%',
+    top: '32px',
+    textAlign: 'center',
+    color: 'rgb(24, 144, 255)',
+    overflow: 'hidden',
+  } as Properties,
+  titleContainer: {
+    width: '100%',
+    padding: '18px 0px',
+    textAlign: 'center',
+    color: 'rgb(24, 144, 255)',
+    overflowX: 'hidden',
+  } as Properties,
+  titleText: {
+    fontFamily: 'Avenir',
+    fontSize: '24px',
+    fontWeight: 600,
+  } as Properties,
+  subtitleText: {
+    fontFamily: 'Avenir',
+    fontSize: '12px',
+  } as Properties,
+};
