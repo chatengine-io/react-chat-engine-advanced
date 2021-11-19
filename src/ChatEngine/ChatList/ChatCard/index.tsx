3291bb3f1c4f78eb57583a48f7cc57155581dec5
Adam La Morre <alamorre@gmail.com>
2021-11-18 20:22:40 -0800

    better component structure with Chat Card

--- src/ChatEngine/ChatList/ChatCard/index.tsx
+++ src/ChatEngine/ChatList/ChatCard/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Properties } from 'csstype';
 
 import { Props } from './props';
 import { styles } from './styles';
@@ -12,114 +13,94 @@ export const ChatCard: React.FC<Props> = ({
   description = '',
   timeStamp = '',
   onClick,
-  customStyle = {},
+  chatCardStyle = {},
+  hoveredChatCardStyle = {},
+  activeChatCardStyle = {},
+  chatCardTitleStyle = {},
+  chatCardNotificationStyle = {},
+  chatCardSubtitleStyle = {},
+  chatCardTimeStampStyle = {},
+  chatCardLoadingBarStyle = {},
 }: Props) => {
   const [hovered, setHovered] = useState<boolean>(false);
 
-  const titleWidth = hasNotification ? 'calc(100% - 18px)' : '100%';
-  const hoverStyle = hovered && styles.hoveredChat;
-  const activeStyle = isActive && styles.activeChat;
+  const loadingBarStyle: Properties = isLoading
+    ? { ...styles.chatCardLoadingBarStyle, ...chatCardLoadingBarStyle }
+    : {};
+  const hasNotificationStyle: Properties = hasNotification
+    ? {}
+    : { display: 'none' };
 
   return (
     <div
       onClick={onClick}
-      style={{
-        ...styles.chatCardContainer,
-        ...hoverStyle,
-        ...activeStyle,
-        ...customStyle.chatCardContainer,
-      }}
-      className={`ce-chat-card ${isActive && 'ce-active-chat-card'}`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
+      style={{
+        // Default
+        ...styles.chatCardStyle,
+        // State
+        ...(hovered ? styles.hoveredChatCardStyle : {}),
+        ...(isActive ? styles.activeChatCardStyle : {}),
+        // Props
+        ...chatCardStyle,
+        // Props + State
+        ...(hovered ? hoveredChatCardStyle : {}),
+        ...(isActive ? activeChatCardStyle : {}),
+      }}
+      className={`
+        ce-chat-card 
+        ${isActive && 'ce-active-chat-card'} 
+        ${hovered && 'ce-hovered-chat-card'}
+      `}
     >
       <div
-        style={{ ...styles.titleText, ...customStyle.titleText }}
-        className="ce-chat-title-text"
+        className="ce-chat-card-title"
+        style={{
+          ...styles.chatCardTitleStyle,
+          ...loadingBarStyle,
+          ...chatCardTitleStyle,
+        }}
         id={`ce-chat-card-title-${title}`}
       >
-        <div
-          style={{
-            width: titleWidth,
-            overflowX: 'hidden',
-            display: 'inline-block',
-          }}
-        >
-          {!isLoading ? (
-            title
-          ) : (
-            <div
-              className="ce-chat-card-loading-bar"
-              style={{
-                ...styles.loadingBar,
-                ...{ width: '99%' },
-                ...customStyle.loadingBar,
-              }}
-            />
-          )}
-        </div>
-
-        {hasNotification && (
-          <div
-            className="ce-chat-unread-dot"
-            style={{
-              marginTop: '5px',
-              width: '12px',
-              height: '12px',
-              borderRadius: '6px',
-              backgroundColor: '#1890ff',
-              float: 'right',
-              display: 'inline-block',
-            }}
-          />
-        )}
+        {isLoading ? '.' : title}
       </div>
 
-      <div style={{ width: '100%' }} className="ce-chat-subtitle">
-        <div
-          style={{ ...styles.messageText, ...customStyle.messageText }}
-          className="ce-chat-subtitle-text ce-chat-subtitle-message"
-        >
-          {!isLoading ? (
-            <div style={{ height: '19px', overflow: 'hidden' }}>
-              <div dangerouslySetInnerHTML={{ __html: description }} />
-              <style>{`p {margin-block-start: 0px; margin-block-end: 0px;}`}</style>
-            </div>
-          ) : (
-            <div
-              className="ce-chat-card-loading-bar"
-              style={{
-                ...styles.loadingBar,
-                ...{ width: '60%' },
-                ...customStyle.loadingBar,
-              }}
-            />
-          )}
-        </div>
+      <div
+        className="ce-chat-card-unread"
+        style={{
+          ...styles.chatCardNotificationStyle,
+          ...hasNotificationStyle,
+          ...loadingBarStyle,
+          ...chatCardNotificationStyle,
+        }}
+      />
 
+      <div
+        className="ce-chat-card-subtitle"
+        style={{
+          ...styles.chatCardSubtitleStyle,
+          ...loadingBarStyle,
+          ...chatCardSubtitleStyle,
+        }}
+      >
         <div
-          className="ce-chat-subtitle-text ce-chat-subtitle-date"
-          style={{
-            ...styles.messageText,
-            ...{ textAlign: 'right', width: '25%' },
+          dangerouslySetInnerHTML={{
+            __html: isLoading ? '.' : description,
           }}
-        >
-          {!isLoading ? (
-            timeStamp
-          ) : (
-            <div
-              className="ce-chat-card-loading-bar"
-              style={{
-                ...styles.loadingBar,
-                ...{
-                  width: 'calc(100% - 5px)',
-                  position: 'relative',
-                  right: '5px',
-                },
-              }}
-            />
-          )}
-        </div>
+        />
+        <style>{`p {margin-block-start: 0px; margin-block-end: 0px;}`}</style>
+      </div>
+
+      <div
+        className="ce-chat-card-time-stamp"
+        style={{
+          ...styles.chatCardTimeStampStyle,
+          ...loadingBarStyle,
+          ...chatCardTimeStampStyle,
+        }}
+      >
+        {isLoading ? '.' : timeStamp}
       </div>
     </div>
   );
