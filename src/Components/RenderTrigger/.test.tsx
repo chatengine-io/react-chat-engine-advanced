d34a510b047efddd830370141f5fd3950303d929
Adam La Morre <alamorre@gmail.com>
2021-11-23 16:24:49 -0800

    added io to test

--- src/Components/RenderTrigger/.test.tsx
+++ src/Components/RenderTrigger/.test.tsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import { RenderTrigger as Thing } from '.';
+
+import 'intersection-observer';
+
 describe('Thing', () => {
-  it('renders without crashing', () => {});
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Thing />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
