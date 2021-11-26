8973a8c267d4efb838dca5bca39b00c7629aa96e
Adam La Morre <alamorre@gmail.com>
2021-11-26 14:53:03 -0800

    Feat spinner component (#61)
    * edits to new script
    
    * spinner is done

---
+++ src/Components/Spinner/styles.tsx
@@ -0,0 +1,9 @@
+import { Properties } from 'csstype';
+
+export interface SpinnerStyles {
+  style?: Properties;
+}
+
+export const styles: SpinnerStyles = {
+  style: { width: '1em', height: '1em' },
+};
