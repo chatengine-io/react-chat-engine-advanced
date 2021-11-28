3bd0f084f18862e9cad2980e0874ffc331b3740b
Adam La Morre <alamorre@gmail.com>
2021-11-28 12:20:18 -0800

    chat form is done

--- src/ChatEngine/ChatList/ChatForm/props.tsx
+++ src/ChatEngine/ChatList/ChatForm/props.tsx
@@ -3,5 +3,8 @@ import { HTMLAttributes } from 'react';
 import { ChatFormStyles } from './styles';
 
 export interface Props extends HTMLAttributes<HTMLFormElement>, ChatFormStyles {
+  // Hooks
   onFormSubmit?: (value: string) => void;
+  // Render Functions
+  renderChatForm?: (props: Props) => React.FC<Props>;
 }
