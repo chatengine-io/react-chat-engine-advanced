9ae61685f4862a673aa0d2b782b8071f249945ed
Adam La Morre <alamorre@gmail.com>
2021-11-28 12:39:22 -0800

    chat list is done

--- src/ChatEngine/ChatList/props.tsx
+++ src/ChatEngine/ChatList/props.tsx
@@ -4,13 +4,23 @@ import { ChatListStyles } from './styles';
 
 import { ChatsProps } from '../../util/interfaces';
 
+import { Props as ChatFormProps } from './ChatForm/props';
+import { Props as ChatCardProps } from './ChatCard/props';
+
 export interface Props extends HTMLAttributes<HTMLDivElement>, ChatListStyles {
+  // Data
   chats: ChatsProps;
   activeChatKey?: number;
   myUsername?: string;
+  // State
   isLoading?: boolean;
   hasMoreChats?: boolean;
+  // Hooks
   onChatCardClick?: (chatId: number) => void;
   onChatFormSubmit?: (title: string) => void;
   onChatLoaderVisible?: () => void;
+  // Render Functions
+  renderChatList?: (props: Props) => React.FC<Props>;
+  renderChatForm?: (props: ChatFormProps) => React.FC<ChatFormProps>;
+  renderChatCard?: (props: ChatCardProps) => React.FC<ChatCardProps>;
 }
