ff0bd51c298e0e8c7685112bd3a566004ace509e
Adam La Morre <alamorre@gmail.com>
2021-12-01 08:36:02 -0800

    input and spinner is done

--- src/Components/Input/props.tsx
+++ src/Components/Input/props.tsx
@@ -1,13 +1,10 @@
-import React, { HTMLAttributes } from 'react';
+import { HTMLAttributes } from 'react';
 
 import { InputStyles } from './styles';
 
 export interface Props extends HTMLAttributes<HTMLInputElement>, InputStyles {
-  autoFocus?: boolean;
   label?: string;
   defaultValue?: string | undefined;
   value?: string | undefined;
-  onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
-  onFocus?: React.FocusEventHandler<HTMLInputElement> | undefined;
-  onBlur?: React.FocusEventHandler<HTMLInputElement> | undefined;
+  autoFocus?: boolean;
 }
--- src/Components/Input/styles.tsx
+++ src/Components/Input/styles.tsx
@@ -4,6 +4,19 @@ export interface InputStyles {
 }
 
 export const styles: InputStyles = {
+  style: {
+    fontFamily: 'Avenir',
+    height: '36px',
+    fontSize: '15px',
+    outline: 'none',
+    borderRadius: '24px',
+    border: '1px solid #d9d9d9',
+    padding: '0px 12px',
+    boxSizing: 'border-box',
+    transition: 'all .33s ease',
+    WebkitTransition: 'all .33s ease',
+    MozTransition: 'all .33s ease',
+  },
   focusStyle: {
     border: '1px solid #1890ff',
   },
