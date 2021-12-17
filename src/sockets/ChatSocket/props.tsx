031c65919138e9308c1f784741648f5d9ad48ff4
Adam La Morre <alamorre@gmail.com>
2021-12-16 18:18:25 -0800

    Chat and User Socket (#119)

---
+++ src/sockets/ChatSocket/props.tsx
@@ -0,0 +1,17 @@
+import { ChatProps, MessageProps, PersonProps } from '../../interfaces';
+
+export interface Props {
+  projectId?: string;
+  chatId?: number | string;
+  chatAccessKey?: string;
+  onConnect?: () => void;
+  onError?: () => void;
+  onClose?: () => void;
+  onRefresh?: () => void;
+  onEditChat?: (chat: ChatProps) => void;
+  onDeleteChat?: (chat: ChatProps) => void;
+  onNewMessage?: (chatId: number, message: MessageProps) => void;
+  onEditMessage?: (chatId: number, message: MessageProps) => void;
+  onDeleteMessage?: (chatId: number, message: MessageProps) => void;
+  onIsTyping?: (chatId: number, person: PersonProps) => void;
+}
