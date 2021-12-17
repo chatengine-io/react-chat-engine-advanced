34072331ec71b655c6b7db89625676145be199a0
Adam La Morre <alamorre@gmail.com>
2021-12-16 18:13:56 -0800

    Chat and User Socket

---
+++ src/sockets/ChatSocket/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import { Props } from './props';
+
+import { ChildSocket } from './childSocket';
+
+export const Socket: React.FC<Props> = (props: Props) => {
+  const [isHidden, setIsHidden] = useState(false);
+
+  function reRender() {
+    setIsHidden(true);
+    setTimeout(() => setIsHidden(false), 100);
+  }
+
+  if (isHidden) return <div />;
+
+  return (
+    <ChildSocket
+      projectId={props.projectId}
+      chatId={props.chatId}
+      chatAccessKey={props.chatAccessKey}
+      onRefresh={reRender}
+      onConnect={props.onConnect}
+      onError={props.onError}
+      onClose={props.onClose}
+      onEditChat={props.onEditChat}
+      onDeleteChat={props.onDeleteChat}
+      onNewMessage={props.onNewMessage}
+      onEditMessage={props.onEditMessage}
+      onDeleteMessage={props.onDeleteMessage}
+      onIsTyping={props.onIsTyping}
+    />
+  );
+};
--- src/sockets/Socket/props.tsx
+++ src/sockets/Socket/props.tsx
@@ -1,4 +1,4 @@
-import { ChatProps, MessageProps, PersonProps } from '../interfaces';
+import { ChatProps, MessageProps, PersonProps } from '../../interfaces';
 
 export interface Props {
   projectId?: string;
