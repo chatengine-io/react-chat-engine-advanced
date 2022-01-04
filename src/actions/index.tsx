ab2a4296097653bee081b3f06816c9195f524791
Adam La Morre <alamorre@gmail.com>
2022-01-04 07:50:22 -0800

    refactor: get or create chat

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -5,10 +5,9 @@ export * from './messages/readMessage';
 export * from './chats/getChat';
 export * from './chats/deleteChat';
 export * from './chats/getChatsBefore';
+export * from './chats/getOrCreateChat';
 export * from './chats/newChat';
 
 export * from './people/getPeopleToInvite';
 export * from './people/invitePerson';
 export * from './people/removePerson';
-
-export { getOrCreateChat } from './deprecated/getOrCreateChat';
