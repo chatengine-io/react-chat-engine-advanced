34b430123051414c53fde91e7248f56e7a3342ac
Adam La Morre <alamorre@gmail.com>
2022-01-08 09:29:13 -0800

    component rename

--- example-ts/src/App.tsx
+++ example-ts/src/App.tsx
@@ -4,19 +4,20 @@ import {
   MyChatsSocket,
   ChatWindow,
   useMyChatsLogic,
-} from 'react-chat-engine-components';
+} from 'react-chat-engine-advanced';
 
 const projectId = '1ed59673-1fd6-46ed-9eb9-56239a6a4f82';
 const myUsername = 'Adam_La_Morre';
 const mySecret = 'pass1234';
 
 const App: React.FC = () => {
-  const state = useMyChatsLogic(projectId, myUsername, mySecret, true);
+  const chatProps = useMyChatsLogic(projectId, myUsername, mySecret, true);
+
   return (
-    <div>
-      <MyChatsSocket {...state} />
-      <ChatWindow timezoneOffset={1} {...state} />
-    </div>
+    <>
+      <MyChatsSocket {...chatProps} />
+      <ChatWindow timezoneOffset={1} {...chatProps} />
+    </>
   );
 };
 
