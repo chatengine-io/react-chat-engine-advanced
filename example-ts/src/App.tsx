66415d0cab1602d44090c98ea19ccc8cab9b14c7
Adam La Morre <alamorre@gmail.com>
2022-06-29 12:22:18 -0400

    0.1.12

--- example-ts/src/App.tsx
+++ example-ts/src/App.tsx
@@ -6,9 +6,9 @@ import {
   useMultiChatLogic,
 } from 'react-chat-engine-advanced';
 
-const projectId = '1ed59673-1fd6-46ed-9eb9-56239a6a4f82';
-const username = 'Adam_La_Morre';
-const secret = 'pass1234';
+const projectId = '05563c5e-ccbb-41fd-bb1f-bbf5cdd83272';
+const username = 'adam@lamorre.co';
+const secret = 'Pass1234!';
 
 const App: React.FC = () => {
   const chatProps = useMultiChatLogic(projectId, username, secret);
